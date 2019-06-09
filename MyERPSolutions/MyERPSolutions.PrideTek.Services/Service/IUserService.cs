b6ccc242f0041447ba71d4b5083af4207b16c48a
Van Le <van@pridetek.com>
2019-06-09 15:53:48 -0700

    Commit before adding User Controller

---
+++ MyERPSolutions/MyERPSolutions.PrideTek.Services/Service/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using MyERPSolutions.PrideTek.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyERPSolutions.PrideTek.Services.Service
+{
+    interface IUserService
+    {
+        Task<User> GetUserById(long id);
+    }
+}
---
+++ MyERPSolutions/MyERPSolutions.PrideTek.Services/ServiceImp/UserService.cs
@@ -0,0 +1,25 @@
+﻿using MyERPSolutions.PrideTek.Dao.Dao;
+using MyERPSolutions.PrideTek.Models;
+using MyERPSolutions.PrideTek.Services.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyERPSolutions.PrideTek.Services.ServiceImp
+{
+    public class UserService : IUserService
+    {
+        private IGenericRepository _repository;
+        public UserService(IGenericRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public async Task<User> GetUserById(long id)
+        {
+            var user = await this._repository.GetByIdAsync<User>(id);
+            return user;
+        }
+    }
+}
